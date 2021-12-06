28ab2539ea74141489dbb9e253f897caa20b20d6
Tony <zgh.1210@icloud.com>
2021-12-06 18:27:55 +0800

    add pattern graph

--- graph/tests/test_basic_graph.cc
+++ graph/tests/test_basic_graph.cc
@@ -2,15 +2,33 @@
 
 using namespace algorithm::graph;
 
-int main() {
 
+/* Create kinds of graph
+ *      A
+ *     /  \
+ *    B    C
+ *     \  /
+ *       D
+ */
+template<typename Ty>
+Graph<Node<Ty>, Edge<Ty>> create_pattern1_graph() {
     Graph<Node<int>, Edge<int>> graph;
-    auto node_tmp = std::make_shared<Node<int>>("B");
     auto node_a = graph.AddNode("A");
     auto node_b = graph.AddNode("B");
+    auto node_c = graph.AddNode("C");
+    auto node_d = graph.AddNode("D");
     graph.AddEdge(node_a, node_b);
-    graph.DumpNodes();
-    graph.DumpEdges();
+    graph.AddEdge(node_a, node_c);
+    graph.AddEdge(node_b, node_d);
+    graph.AddEdge(node_c, node_d);
+    return graph;
+}
+
+int main() {
+
+    auto graph_pattern0 = create_pattern1_graph<int>();
+    graph_pattern0.DumpNodes();
+    graph_pattern0.DumpEdges();
     return 0;
 }
 
